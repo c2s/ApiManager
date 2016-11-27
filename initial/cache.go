9d294da713dd0b6b17a00d820a0e69168e89aa33
qingsong <qingsongr@gmail.com>
2016-11-27 19:31:32 +0800

    Api Manage Init

---
+++ initial/cache.go
@@ -0,0 +1,14 @@
+// Copyright 2016 ApiManager Author. All Rights Reserved.
+// Author Mofree<mofree@mofree.org>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// license that can be found in the LICENSE file.
+
+package initial
+
+import (
+	"ApiManager/utils"
+)
+
+func InitCache() {
+	utils.InitCache()
+}
---
+++ initial/template.go
@@ -0,0 +1,62 @@
+// Copyright 2016 ApiManager Author. All Rights Reserved.
+// Author Mofree<mofree@mofree.org>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// license that can be found in the LICENSE file.
+
+package initial
+
+import (
+	//"fmt"
+	"ApiManager/models/businesstrips"
+	"ApiManager/models/expenses"
+	"ApiManager/models/goouts"
+	"ApiManager/models/leaves"
+	"ApiManager/models/oagoods"
+	"ApiManager/models/overtimes"
+	"ApiManager/models/projects"
+	"ApiManager/models/users"
+	"ApiManager/utils"
+	//"time"
+
+	"github.com/astaxie/beego"
+)
+
+func InitTemplate() {
+	beego.AddFuncMap("getRealname", users.GetRealname)
+	beego.AddFuncMap("getNeedsname", projects.GetProjectNeedsName)
+	beego.AddFuncMap("getProjectname", projects.GetProjectName)
+	beego.AddFuncMap("getLeaveProcess", leaves.ListLeaveApproverProcessHtml)
+	beego.AddFuncMap("getExpenseProcess", expenses.ListExpenseApproverProcessHtml)
+	beego.AddFuncMap("getBusinesstripProcess", businesstrips.ListBusinesstripApproverProcessHtml)
+	beego.AddFuncMap("getGooutProcess", goouts.ListGooutApproverProcessHtml)
+	beego.AddFuncMap("getOagoodProcess", oagoods.ListOagoodApproverProcessHtml)
+	beego.AddFuncMap("getOvertimeProcess", overtimes.ListOvertimeApproverProcessHtml)
+
+	beego.AddFuncMap("getDate", utils.GetDate)
+	beego.AddFuncMap("getDateMH", utils.GetDateMH)
+	beego.AddFuncMap("getNeedsStatus", utils.GetNeedsStatus)
+	beego.AddFuncMap("getNeedsSource", utils.GetNeedsSource)
+	beego.AddFuncMap("getNeedsStage", utils.GetNeedsStage)
+	beego.AddFuncMap("getTaskStatus", utils.GetTaskStatus)
+	beego.AddFuncMap("getTaskType", utils.GetTaskType)
+	beego.AddFuncMap("getTestStatus", utils.GetTestStatus)
+	beego.AddFuncMap("getLeaveType", utils.GetLeaveType)
+
+	beego.AddFuncMap("getOs", utils.GetOs)
+	beego.AddFuncMap("getBrowser", utils.GetBrowser)
+	beego.AddFuncMap("getAvatarSource", utils.GetAvatarSource)
+	beego.AddFuncMap("getAvatar", utils.GetAvatar)
+	beego.AddFuncMap("getAvatarUserid", users.GetAvatarUserid)
+	beego.AddFuncMap("getPositionsName", users.GetPositionsNameForUserid)
+	beego.AddFuncMap("getDepartmentsName", users.GetDepartmentsNameForUserid)
+
+	beego.AddFuncMap("getEdu", utils.GetEdu)
+	beego.AddFuncMap("getWorkYear", utils.GetWorkYear)
+	beego.AddFuncMap("getResumeStatus", utils.GetResumeStatus)
+
+	beego.AddFuncMap("getCheckworkType", utils.GetCheckworkType)
+
+	beego.AddFuncMap("getMessageType", utils.GetMessageType)
+	beego.AddFuncMap("getMessageSubtype", utils.GetMessageSubtype)
+
+}
